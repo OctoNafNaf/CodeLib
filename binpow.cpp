6a770a5543b1ff184f5c8549c0b8f4ba71799660
Nikita <snikers_95@bk.ru>
2012-10-19 19:39:12 +0300

    Update binpow.cpp

--- binpow.cpp
+++ binpow.cpp
@@ -14,9 +14,21 @@ ll binpow(int a, int x) {
     return r;
 }
 
+ll binpowmod(int a, int x, ll mod) {
+    ll r = 1, p = a;
+    while (x > 0) {
+        if (x & 1)
+            r = (r * p) % mod;
+        x >>= 1;
+        p = (p * p) % mod;
+    }
+    return r % mod;
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << binpow(a, b);
+    cout << binpow(a, b) << endl;
+    cout << binpowmod(a, b, (int)(1e9) + 7);
     return 0;
 }
