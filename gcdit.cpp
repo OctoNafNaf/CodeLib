49400e4b29befe4716193d6a18161dbccbd9ea66
Valery Tolstov <testautomail@mail.ru>
2012-10-19 21:10:10 +0400

    Merge files with GCD

--- gcdit.cpp
+++ gcdit.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 typedef long long ll;
 
-template <class T> T GCD(T a, T b) {
+template <class T>
+T gcdxor(T a, T b) {
     while (a) a^=b^=a^=b%=a; return b;
 }
 
-int gcd(int a, int b) {
+template <class T>
+T gcd(T a, T b) {
     while (b > 0) {
         a %= b;
         swap(a, b);
@@ -16,9 +18,18 @@ int gcd(int a, int b) {
     return a;
 }
 
+template <class T>
+T gcdrec(T a, T b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << gcd(a, b) << endl << GCD(a, b);
+    cout << gcd(a, b) << endl
+         << gcdxor(a, b) << endl
+         << gcdrec(a, b) << endl;
     return 0;
 }
